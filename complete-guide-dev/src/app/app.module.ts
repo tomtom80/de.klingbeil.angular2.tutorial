b8864872903806df96d84b05ca961e6bc83d31b7
tomtom80 <tom.klingbeil@gmail.com>
2017-01-24 18:37:02 +0100

    kapitel 25

--- complete-guide-dev/src/app/app.module.ts
+++ complete-guide-dev/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DatabindingComponent } from './databinding/databinding.component';
@@ -14,7 +15,8 @@ import { EventBindingComponent } from './databinding/event-binding.component';
     EventBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
